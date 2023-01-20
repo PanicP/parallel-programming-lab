3e9efb067086749e5e8982f683e68f3107947e71
PanicP <filmous2136@gmail.com>
2023-01-20 15:48:28 +0100

    sadf

--- MPI/IO/mpi_io.c
+++ MPI/IO/mpi_io.c
@@ -12,21 +12,23 @@ int main(int argc, char **argv)
     char charToWritten = '0' + rank;
 
     // Open File
-    MPI_File_open(MPI_COMM_WORLD, "test.out",
-                  MPI_MODE_CREATE | MPI_MODE_WRONLY,
-                  MPI_INFO_NULL, &fh);
+    MPI_File fh;
+    MPI_File_open(MPI_COMM_WORLD, "test.out", MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fh);
+
     // Calculate displacement (offset from the begining)
-    int displacement = ;
+    int displacement = 6 * sizeof(int);
 
     // Calculate offset (jump to next position)
-    int offset = ;
+    int offset = 5 * sizeof(int);
 
     for (int i = 0; i < 5; i++)
     {
+        MPI_File_write_at(&fh, offset, displacement, count, );
         // write to file AT correct position
     }
 
     // close file
+    MPI_File_close(&fh);
 
     MPI_Finalize();
     return 0;
