b1f66e97e23e7fb8b578253fc2ed24924d1f3839
PanicP <filmous2136@gmail.com>
2023-01-20 16:36:39 +0100

    asdf

--- MPI/IO/mpi_io2.c
+++ MPI/IO/mpi_io2.c
@@ -18,14 +18,14 @@ int main(int argc, char **argv)
 
     MPI_Datatype elementType = MPI_CHAR;
     int ndims = 1;
-    int array_of_size[1] = {3};
+    int array_of_size[1] = {size};
     int array_of_subsizes[1] = {1};
     int array_of_starts[1] = {rank};
 
     MPI_Datatype fileType;
     MPI_Type_create_subarray(ndims, array_of_size, array_of_subsizes, array_of_starts, MPI_ORDER_C, elementType, &fileType);
     MPI_Type_commit(&fileType);
-    MPI_File_set_view(fh, 0, elementType, filetype, 'native', MPI_INFO_NULL)
+    MPI_File_set_view(fh, 0, elementType, fileType, "native", MPI_INFO_NULL);
 
         // Calculate displacement (offset from the begining)
         // int displacement = rank;
